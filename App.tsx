d23288a1af289e57a677671aea70aa4a28f9892b
Guillermo Rivas <grivas@pop-os.localdomain>
2022-05-17 10:31:51 -0500

    clase de mayo 17

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Dino, { ComponenteFuncional, styles } from './classes/Perrito';
 import CicloDeVida from './classes/CicloDeVida';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [displayChildren, setDisplayChildren] = useState(true);
+  const [cuentita, setCuentita] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Dino></Dino>
       <ComponenteFuncional></ComponenteFuncional>
-      <CicloDeVida></CicloDeVida>
+      {
+        displayChildren &&
+        <CicloDeVida apellido="Martinez" edad={cuentita}></CicloDeVida>
+      }
+      <Button 
+          title="OCULTAR / MOSTRAR"
+          onPress={() => {
+            setDisplayChildren(!displayChildren);
+          }}
+      />
+      <Button 
+          title="ACTUALIZAR CUENTA"
+          onPress={() => {
+            setCuentita(cuentita + 1);
+          }}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- classes/CicloDeVida.tsx
+++ classes/CicloDeVida.tsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Button, Text } from 'react-native';
 
 // qué es el ciclo de vida
 // - métodos que se definen como parte de un componente
 // - se ejecutan de manera "automatica" durante la vida del componente
 
 export default class CicloDeVida extends Component {
+
+    async request() {
+
+        var respuesta = await fetch("https://raw.githubusercontent.com/gmorivastec/TC3005B_FRONTEND_APR4/master/gatitos.json");
+        var json = await respuesta.json();
+        
+        this.setState((state)=>{
+            console.log("DETONANDO CAMBIO DE ESTADO");    
+            return {name: json[0].nombre};
+        });
+        
+    }
 
     // 3 momentos grandes en la vida de un componente
     // - montaje (mounting)
@@ -17,21 +29,81 @@ export default class CicloDeVida extends Component {
     constructor(props: any){
         // lo primero que hacemos es invocar al superconstructor
         super(props);
+
         console.log("CONSTRUCTOR!");
         // lo que quieras va aquí!
+
+        // state 
+        // ¿podríamos utilizar variables de instancia?
+        // ¿por qué usar state entonces?
+        // ya existe el atributo this.state
+        // recomendaciones - sólo escribirlo directament en el constructor
+        this.state = {count: 0, name: "componentito"};
+        this.request();
+        // posteriormente siempre usaremos setState
+        
     }
 
     // esto corre después una vez PERO también vive en update
     render(){
-        console.log("RENDER!");
+        console.log("RENDER!" + this.props.apellido);
         return(
-            <Text>HOLA SOY EL CICLO DE VIDA</Text>
+            <View>
+                <Text>HOLA SOY EL CICLO DE VIDA Y ME LLAMO {this.state.name} {this.props.edad} ADEMÁS {this.state.count}</Text>
+                <Button 
+                    title="CAMBIAR NOMBRE"
+                    onPress={() => {
+                        this.setState((state) => {
+                            return {name: "componentote"};
+                        });
+                    }}
+                />
+                <Button 
+                    title="CAMBIAR CUENTA"
+                    onPress={() => {
+                        this.setState((state, props) => {
+                            return {count: state.count + 1};
+                        });
+                    }}
+                />
+            </View>
         );
     }
 
     // una vez que se hizo render y todo fue exitoso
     componentDidMount() {
         console.log("SE MONTÓ EL COMPONENTE EXITOSAMENTE!");
+    }
+
+    // 2DO MOMENTO - UPDATE 
+    // se detona cuando hay cambio de estado / propiedades
+
+    // 1ero - verificamos si hay cambio
+    // PODEMOS sobreescribir el método filtro
+    shouldComponentUpdate(nextProp: any, nextState: any){
+
+        // recibimos - nuevos props y nuevos states
+        // podemos comparar vs. anterior
+        console.log("SHOULD COMPONENT UPDATE INVOCADO (state) " + this.state.count + " VS. " + nextState.count);
+        console.log("SHOULD COMPONENT UPDATE INVOCADO (props) " + this.props.edad + " VS. " + nextProp.edad);
+        
+        // siempre regresa true por default
+        return true;
+    }
+
+    // render también viene como parte del update 
+
+    // último método invocado una vez que se hizo el render 
+    componentDidUpdate(prevProp: any, prevState: any){
+        console.log("COMPONENT DID UPDATE INVOCADO " + this.state.count + " VS. " + prevState.count);
+    }
+
+    // 3ER MOMENTO
+    // desmontaje - quitamos componente de vista
+    componentWillUnmount() {
+
+        // aquí limpiamos servicios / subscripciones que estén activos
+        console.log("COMPONENT WILL UNMOUNT INVOCADO");
     }
 
     // EXTRA!
